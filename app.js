3db66e693ce9e7de21d0cdec57e3294989083839
Etienne le Roux <efleroux@gmail.com>
2020-03-29 11:11:40 +0200

    odds

--- app.js
+++ app.js
@@ -3,7 +3,20 @@ const app = express();
 const bodyParser = require("body-parser");
 const port = 5400;
 
+
+var documents = [
+    {name: "Police clearance", image: "https://image.freepik.com/free-photo/pile-unfinished-documents-office-desk_42667-858.jpg"},
+    {name: "Police clearance expungement", image: "https://image.freepik.com/free-photo/pile-unfinished-documents-office-desk_42667-858.jpg"},
+    {name: "Abridged Marriage certificate", image: "https://image.freepik.com/free-photo/pile-unfinished-documents-office-desk_42667-858.jpg"},
+    {name: "Unabridged Marriage Certificate", image: "https://image.freepik.com/free-photo/pile-unfinished-documents-office-desk_42667-858.jpg"},
+    {name: "Abridged Birth certificate", image: "https://image.freepik.com/free-photo/pile-unfinished-documents-office-desk_42667-858.jpg"},
+    {name: "Unabridged Birth Certificate", image: "https://image.freepik.com/free-photo/pile-unfinished-documents-office-desk_42667-858.jpg"},
+    {name: "Unabridged Marriage Certificate", image: "https://image.freepik.com/free-photo/pile-unfinished-documents-office-desk_42667-858.jpg"}
+];
+
+//to use body parser
 app.use(bodyParser.urlencoded({extended: true}));
+//view engine
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
@@ -11,25 +24,23 @@ app.get("/", (req, res) => {
 });
 
 app.get("/documents", (req, res)=> {
-    var documents = [
-        {name: "Police clearance", image: "https://image.freepik.com/free-photo/pile-unfinished-documents-office-desk_42667-858.jpg"},
-        {name: "Police clearance expungement", image: "https://image.freepik.com/free-photo/pile-unfinished-documents-office-desk_42667-858.jpg"},
-        {name: "Abridged Marriage certificate", image: "https://image.freepik.com/free-photo/pile-unfinished-documents-office-desk_42667-858.jpg"},
-        {name: "Unabridged Marriage Certificate", image: "https://image.freepik.com/free-photo/pile-unfinished-documents-office-desk_42667-858.jpg"},
-        {name: "Abridged Birth certificate", image: "https://image.freepik.com/free-photo/pile-unfinished-documents-office-desk_42667-858.jpg"},
-        {name: "Unabridged Birth Certificate", image: "https://image.freepik.com/free-photo/pile-unfinished-documents-office-desk_42667-858.jpg"},
-        {name: "Unabridged Marriage Certificate", image: "https://image.freepik.com/free-photo/pile-unfinished-documents-office-desk_42667-858.jpg"}
-    ]
     res.render("documents", {documents:documents});
 });
 
 app.post("/documents", (req, res) => {
-    res.send("You hit the post route");
     //get data from from form and add to documents array
-    req.body.name
-    req.body.image
+    var name = req.body.name;
+    var image = req.body.image;
+    var newDocument = {name: name, image: image}
+    documents.push(newDocument);
+    res.redirect("/documents");
     //redirect to documents page
 });
+
+//shows form which sends data to db
+app.get("/documents/new", (req, res) => {
+        res.render("new.ejs");
+})
 
 app.listen(port, () => {
     console.log("Docs2go application has started!")
